6eb3cb62e08ca305a1bdc79038908f53f8b10578
kh3rm <h3rmanoo@gmail.com>
2025-04-15 01:16:58 +0200

    First commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>
+            <h1>
+            Wheel of fortune
+            </h1>
+            <h2><i>Lucky number:</h2>
+
+            <h2>'.$number.' </i></h2>
+            <h2>☝</h2>
+            </body></html>'
+        );
+    }
+
+
+    #[Route("/lucky/hi")]
+    public function hi(): Response
+    {
+        return new Response(
+            '<html><body>
+            <h1>
+            📣
+            </h1>
+            <h2>
+            Hi to you, sir/madam!</body></html>
+            </h2>'
+        );
+    }
+}
---
+++ src/Controller/LuckyControllerTwig.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerTwig extends AbstractController
+{
+    #[Route("/lucky", name: "lucky")]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        $fruitArray = ["🍎 apple","🍇 grapes","🍌 banana","🍐 pear","🍊 orange","🍒 cherry",
+        "🍓 strawberry","🥝 kiwi","🍍 pineapple","🥭 mango","🍑 peach", "🍉 watermelon"];
+
+        $randomFruitNumber = random_int(0, count($fruitArray) - 1);
+        $randomFruit = $fruitArray[$randomFruitNumber];
+        $data = [
+            'number' => $number,
+            'randomFruit' => $randomFruit
+        ];
+
+        return $this->render('lucky.html.twig', $data);
+    }
+
+
+    #[Route("/", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+}
