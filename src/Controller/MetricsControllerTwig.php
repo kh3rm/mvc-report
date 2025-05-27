25a68b16e46b6e3f0a150f8f892246c950550054
kh3rm <h3rmanoo@gmail.com>
2025-05-28 00:35:51 +0200

    Enable first complete Scrutinizer analysis.

---
+++ src/Controller/MetricsControllerTwig.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MetricsControllerTwig extends AbstractController
+{
+    #[Route("/metrics", name: "metrics")]
+    public function metrics(): Response
+    {
+        return $this->render('metrics.html.twig', $data);
+    }
+
+
+    #[Route("/", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+    #[Route("/api", name: "api")]
+    public function api(): Response
+    {
+        return $this->render('api.html.twig');
+    }
+
+    #[Route("/library", name: "librarylp")]
+    public function library(): Response
+    {
+        return $this->render('book/index.html.twig');
+    }
+}
